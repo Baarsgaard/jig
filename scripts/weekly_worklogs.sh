7e14e7612fa857e0a4430d6cd9b1de0a03ce047d
ste <steff.bpoulsen@gmail.com>
2024-05-17 19:59:23 +0200

    Refactor weekly_worklog example

--- scripts/weekly_worklogs.sh
+++ scripts/weekly_worklogs.sh
@@ -1,60 +1,31 @@
 #!/bin/bash
 
-# Modifier to register worklogs for last week.
+CW_DAY="$(date +%u)" # Current Weekday number: 1-7
 MOD=''
 if [[ "$1" == 'last' ]]; then
+  # Modifier to register worklogs for last week.
   MOD='-7 days'
 fi
 
-CW_DAY="$(date +%u)" # Current Weekday number: 1-7
-
 get_date() {
-  date -d "${1}${MOD}" '+%Y-%m-%d' # Format: 2023-08-20
+  if [[ "$CW_DAY" -gt "$2" ]]; then
+    date -d "last-${1}${MOD}" '+%Y-%m-%d'
+  else
+    date -d "${1}${MOD}" '+%Y-%m-%d'
+  fi
 }
 
-# Monday
-if [[ $CW_DAY -gt 1 ]]; then
-  MON_DATE="$(get_date 'last-monday')"
-else
-  MON_DATE="$(get_date 'monday')"
-fi
-# Tuesday
-if [[ $CW_DAY -gt 2 ]]; then
-  TUE_DATE="$(get_date 'last-tuesday')"
-else
-  TUE_DATE="$(get_date 'tuesday')"
-fi
-# Wednesday
-if [[ $CW_DAY -gt 3 ]]; then
-  WED_DATE="$(get_date 'last-wednesday')"
-else
-  WED_DATE="$(get_date 'wednesday')"
-fi
-# Thursday
-if [[ $CW_DAY -gt 4 ]]; then
-  THU_DATE="$(get_date 'last-thursday')"
-else
-  THU_DATE="$(get_date 'thursday')"
-fi
-# Friday
-if [[ $CW_DAY -gt 5 ]]; then
-  FRI_DATE="$(get_date 'last-friday')"
-else
-  FRI_DATE="$(get_date 'friday')"
-fi
-# Saturday
-if [[ $CW_DAY -gt 6 ]]; then
-  SAT_DATE="$(get_date 'last-saturday')"
-else
-  SAT_DATE="$(get_date 'saturday')"
-fi
-# Sunday
-# No switch as CW_DAY is never greater than 7
-SUN_DATE="$(get_date 'sunday')"
+MON_DATE="$(get_date 'monday' 1)"
+TUE_DATE="$(get_date 'tuesday' 2)"
+WED_DATE="$(get_date 'wednesday' 3)"
+THU_DATE="$(get_date 'thursday' 4)"
+FRI_DATE="$(get_date 'friday' 5)"
+SAT_DATE="$(get_date 'saturday' 6)"
+SUN_DATE="$(get_date 'sunday' 7)"
 
-# Actually create worklogs
-jig log --date "$MON_DATE" 30 JB-129 # Dedicated jira issue for SCRUM meetings or similar.
-# jig log --date "$TUE_DATE" 30 JB-129
+# Create worklogs
+jig log --date "$MON_DATE" 30 JB-129 # Dedicated issue for SCRUM meetings or similar.
+jig log --date "$TUE_DATE" 30 JB-129
 jig log --date "$WED_DATE" 30 JB-129
-# jig log --date "$THU_DATE" 30 JB-129
+jig log --date "$THU_DATE" 30 JB-129
 jig log --date "$FRI_DATE" 1h JB-129
